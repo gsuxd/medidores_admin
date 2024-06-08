90445e75108bab7437b5156d235a94efb4e352ec
gsuxd <jesusmanuelpolancogarcia51@gmail.com>
2024-06-07 20:58:16 -0400

    fix

--- src/models/user/sellerAccount.ts
+++ src/models/user/sellerAccount.ts
@@ -31,6 +31,7 @@ export default class SellerAccount {
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   static fromJson(data: any) {
+    console.log(data)
     return new SellerAccount({
         createdAt: new Date(data['createdAt']),
         id: data['id'],
